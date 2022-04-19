547a54f79a9c9d994e45deda907fc834e34ddc9f
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-04-19 18:55:26 +0630

    File upload to storage directory

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
     public function store(Request $request)
     {
         $filename = time() .'_'. $request->file('image')->getClientOriginalName();
-        $request->file('image')->move(public_path('image'), $filename);
+        //$request->file('image')->move(public_path('image'), $filename);
+        $request->file('image')->storeAs('upload', $filename);
     }
 }
