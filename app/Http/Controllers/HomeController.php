3813e0f5727e348a4ebf439b9d1e0dc4ab752aac
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-04-19 19:17:56 +0630

    Create database, Model and Migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -17,6 +19,10 @@ class HomeController extends Controller
             foreach ($request->file('image') as $image) {
                 $filename = time() .'_'. $image->getClientOriginalName();
                 $image->storeAs('upload', $filename);
+
+                $gallery = new Gallery();
+                $gallery->name = $filename;
+                $gallery->save();
             }
         }
 
